1aea2b9bea6544f93a726a299f81bfcbd9133acb
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-09-08 14:20:45 +0200

    Update launch-ec2.sh

--- misc/launch-ec2.sh
+++ misc/launch-ec2.sh
@@ -2,9 +2,9 @@
 set -e 
 set -u 
 
-X_TYPE=${1:-t2.small}
-X_AMI=${2:-ami-20ca0d59}
-X_DISK=${3:-10}
+X_TYPE=${1:-t2.xlarge}
+X_AMI=${2:-ami-05ea2c7c}
+X_DISK=${3:-100}
 X_SUBNET=${4:-subnet-05222a43} 
 X_RND=$(dd bs=12 count=1 if=/dev/urandom 2>/dev/null | base64 | tr +/ 0A)
 X_MARKER=.launcher-$X_RND
